36ad9276e0f74fcfce800b5d6b842fc9d334032c
UncleThree0402 <wonghokchi0402@gmail.com>
2021-12-23 16:24:00 +0800

    Tidy up RoadController.cs

--- Assets/Scripts/TerrainScripts/RoadController.cs
+++ Assets/Scripts/TerrainScripts/RoadController.cs
@@ -1,25 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadController : MonoBehaviour
 {
-    void Start()
-    {
-    }
-
-
-    void Update()
-    {
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameObject.FindObjectOfType<GroundSpawner>().SpawnTile();
-            Destroy(GameObject.FindObjectOfType<GroundSpawner>().AntepenultimateTile);
+            FindObjectOfType<GroundSpawner>().SpawnTile();
+            Destroy(FindObjectOfType<GroundSpawner>().AntepenultimateTile);
         }
     }
 }
